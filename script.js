08c5143c1d46e77960fa281c77e11746f9ad49be
Umarolimjonov2009 <feruzashokirova1988@gmail.com>
2025-05-03 17:51:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const input=document.getElementById("input");
+const btn=document.getElementById("btn");
+const ul=document.getElementById("ul");
+
+async function view(ism){
+     const response= await fetch(`https://api.nationalize.io/?name=${ism}`);
+     const data= await response.json();
+     ul.innerHTML="";
+     data.country.map(e=>{
+        const li =document.createElement("li");
+        li.innerHTML=`
+         <img src="https://flagcdn.com/20x15/${e.country_id.toLowerCase()}.png" alt="" />   
+         ${e.country_id} - ${(e.probability*100).toFixed(2)}%
+        `;
+        ul.appendChild(li);
+     })
+     
+}
+
+btn.addEventListener("click",()=>{
+    view(input.value)
+})
